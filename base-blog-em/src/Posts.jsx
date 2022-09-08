6eb7f0c3cd2cf6e86b56e766221fd9f83f3f91c3
Blosssom <eju2003@gmail.com>
2022-09-08 20:40:58 +0900

    [EDIT] study useQuery, staleTime, others

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { PostDetail } from "./PostDetail";
+
 const maxPostPage = 10;
 
 async function fetchPosts() {
   const response = await fetch(
     "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
   );
+  // console.log(response);
   return response.json();
 }
 
@@ -14,8 +16,27 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  /**
+   * @param : query의 key와 서버 요청을 위한 비동기 query 함수
+   * @returns : data, isError 등 상태를 반환, 이를 구조분해로 사용할 수 있음
+   */
+  const { data, isLoading, isError, error } = useQuery("posts", fetchPosts, {
+    refetchOnWindowFocus: false,
+    staleTime: 10000,
+  });
+
+  if (isLoading) {
+    return <div>Not data</div>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <div>Oops Error!!!</div>
+        <span>{error.toString()}</span>
+      </>
+    );
+  }
 
   return (
     <>
