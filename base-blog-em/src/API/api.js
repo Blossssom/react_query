835f0837c790fe8dcb3ff703a34efa0151cf2e94
Blosssom <eju2003@gmail.com>
2022-09-11 16:57:23 +0900

    [Edit] add infinite scroll component

--- base-blog-em/src/API/api.js
+++ base-blog-em/src/API/api.js
@@ -1,30 +1,31 @@
 export async function fetchComments(postId) {
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
-    );
-    console.log(response);
-    return response.json();
-  }
-  
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
+  );
+  console.log(response);
+  return response.json();
+}
+
 export async function deletePost(postId) {
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/postId/${postId}`,
-      { method: "DELETE" }
-    );
-    return response.json();
-  }
-  
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    { method: "DELETE" }
+  );
+  console.log(response);
+  return response.json();
+}
+
 export async function updatePost(postId) {
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/postId/${postId}`,
-      { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
-    );
-    return response.json();
-  }
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
+  );
+  return response.json();
+}
 
 export async function fetchPosts(pageNum) {
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
-    );
-    return response.json();
-  }
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
+  );
+  return response.json();
+}
--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,7 +1,8 @@
-import { useQuery } from "react-query";
-import { fetchComments } from "./API/api";
+import { useMutation, useQuery } from "react-query";
+import { deletePost, fetchComments, updatePost } from "./API/api";
 
 export function PostDetail({ post }) {
+  console.log(post.id);
   const { data, isLoading, isError } = useQuery(
     ["fetchComments", post.id],
     () => fetchComments(post.id),
@@ -9,6 +10,9 @@ export function PostDetail({ post }) {
       refetchOnWindowFocus: false,
     }
   );
+
+  const deleteMutation = useMutation((postId) => deletePost(postId));
+  const updateMutation = useMutation((postId) => updatePost(postId));
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -21,7 +25,24 @@ export function PostDetail({ post }) {
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      {deleteMutation.isError && (
+        <p style={{ color: "red" }}>Error delete post</p>
+      )}
+      {deleteMutation.isLoading && (
+        <p style={{ color: "purple" }}>Loading delete post...</p>
+      )}
+      {deleteMutation.isSuccess && (
+        <p style={{ color: "blue" }}>Deleted post...</p>
+      )}
+      <button onClick={() => updateMutation.mutate(post.id)}>
+        Update title
+      </button>
+
+      {updateMutation.isError && <p>Error Update post</p>}
+      {updateMutation.isLoading && <p>Loading update post...</p>}
+      {updateMutation.isSuccess && <p>Success update</p>}
+
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
